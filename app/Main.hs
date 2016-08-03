204e8b209f530b4502b2516ff14d65356bbc35cc
Ashish Negi <ashish@quintype.com>
2016-08-03 22:12:45 +0530

    Using ByteString for communication.

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,18 @@
 module Main where
 
 import Lib
-import Network.Socket
+import Network.Socket hiding (send, sendTo, recv, recvFrom)
+import Network.Socket.ByteString
+import qualified Data.ByteString.Char8 as C
 
 main :: IO ()
 main = do
-  sock <- socket AF_INET Stream 0
+  sock <- socket AF_INET Stream defaultProtocol
   setSocketOption sock ReuseAddr 1
   bind sock (SockAddrInet 4242 iNADDR_ANY)
-  listen sock 2
+  let maxQueuedConnections = 5
+  listen sock maxQueuedConnections
   mainLoop sock
-
 
 mainLoop :: Socket -> IO ()
 mainLoop sock = do
@@ -19,6 +21,6 @@ mainLoop sock = do
   mainLoop sock
 
 runConn :: (Socket, SockAddr) -> IO ()
-runConn (sock, addr) = do
-  send sock "Hello\n"
+runConn (sock, _) = do
+  send sock $ C.pack "Hello and Bye :P \n"
   close sock

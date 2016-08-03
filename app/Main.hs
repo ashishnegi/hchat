dfd9dd10cc8bcc27fd58a6baad0b59dbfd6ed327
Ashish Negi <ashish@quintype.com>
2016-08-03 22:39:07 +0530

    Implements brodcasting of messages.

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,10 @@ import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import Network.Socket.ByteString
 import qualified Data.ByteString.Char8 as C
 import Control.Concurrent
+import Control.Concurrent.Chan
+
+import Control.Monad (liftM)
+import Control.Monad.Fix (fix)
 
 main :: IO ()
 main = do
@@ -12,15 +16,34 @@ main = do
   bind sock (SockAddrInet 4242 iNADDR_ANY)
   let maxQueuedConnections = 5
   listen sock maxQueuedConnections
-  mainLoop sock
+  serverChan <- newChan
+  mainLoop sock serverChan
 
-mainLoop :: Socket -> IO ()
-mainLoop sock = do
+mainLoop :: Socket -> Chan Msg -> IO ()
+mainLoop sock serverChan = do
   conn <- accept sock
-  forkIO $ runConn conn
-  mainLoop sock
+  forkIO $ runConn conn serverChan
+  mainLoop sock serverChan
 
-runConn :: (Socket, SockAddr) -> IO ()
-runConn (sock, _) = do
-  send sock $ C.pack "Hello and Byes :P \n"
+runConn :: (Socket, SockAddr) -> Chan Msg -> IO ()
+runConn (sock, _) serverChan = do
+  send sock $ C.pack "Hello.. Please type text to broadcast.. \n"
+
+  clientChan <- dupChan serverChan
+
+  let maxBytesToRecv = 1024
+
+  forkIO $ fix $ \loop -> do
+    msg <- recv sock maxBytesToRecv
+    writeChan serverChan msg
+    loop
+
+  fix $ \loop -> do
+    msg <- readChan clientChan
+    send sock msg
+    loop
+
   close sock
+
+
+type Msg = C.ByteString

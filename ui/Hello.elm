58362ec293dd888d8dfb1facb06cb67ff178064d
Ashish Negi <ashish@quintype.com>
2016-08-20 20:21:25 +0530

    static page with model,view,update,subscriptions,msg : components of an elm app.

--- ui/Hello.elm
+++ ui/Hello.elm
@@ -1,5 +1,41 @@
 module Hello exposing (..)
 
-import Html exposing (text)
+import Html exposing (Html, text, div)
+import Html.App
 
-main = text "Hello world!!!"
+-- Model
+type alias Model =
+    String
+
+init : (Model, Cmd Msg)
+init = ("Hello", Cmd.none)
+
+-- Messages
+type Msg =
+    NoOp
+
+-- View
+view : Model -> Html Msg
+view model =
+    div []
+        [text model]
+
+-- Update
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        NoOp -> (model, Cmd.none)
+
+-- Subscription
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+main : Program Never
+main =
+    Html.App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }

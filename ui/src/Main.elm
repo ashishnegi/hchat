dd2bc4f6752b82b0b81e645087fdef1b1015d820
Ashish Negi <ashish@quintype.com>
2016-08-24 23:28:08 +0530

    Moved the websocket logic out from User to Main

--- ui/src/Main.elm
+++ ui/src/Main.elm
@@ -1,37 +1,12 @@
-module Hello exposing (..)
+module Main exposing (..)
 
 import Html exposing (Html, text, div, button)
 import Html.Events exposing (onClick)
 import Html.App
 import User.User as User
-
--- Model
-type alias Model =
-    { me : User.Model }
-
-init : (Model, Cmd Msg)
-init = ( Model (User.Model 1 "ashish" "")
-       , Cmd.none)
-
--- Messages
-type Msg = UserMsg User.Msg
-
--- View
-view : Model -> Html Msg
-view model =
-    Html.App.map UserMsg (User.view model.me)
-
--- Update
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model =
-    case msg of
-        UserMsg m -> let ( me', cmds ) = User.update m model.me
-                     in ( { model | me = me' }
-                        , Cmd.map UserMsg cmds )
-
--- Subscription
-subscriptions : Model -> Sub Msg
-subscriptions model = Sub.map UserMsg (User.subscriptions model.me)
+import WebSocket
+import Json.Decode as Decode exposing ((:=))
+import Json.Encode as Encode
 
 main : Program Never
 main =
@@ -41,3 +16,61 @@ main =
         , update = update
         , subscriptions = subscriptions
         }
+
+type alias Model =
+    { me : User.Model }
+
+init : (Model, Cmd Msg)
+init = ( Model (User.Model 1 "ashish" "")
+       , Cmd.none)
+
+type Msg = UserMsg User.Msg
+         | NewMessage Int String String
+         | NoOp
+
+view : Model -> Html Msg
+view model =
+    Html.App.map UserMsg (User.view model.me)
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        UserMsg m ->
+            let ( me', cmds ) = User.update m model.me
+                extraCommand = case m of
+                                   User.SendMsg -> sendMsg me'
+                                   _            -> Cmd.none
+
+            in ( { model | me = me' }
+               , Cmd.batch [ Cmd.map UserMsg cmds
+                           , extraCommand ] )
+
+        _ -> ( model, Cmd.none )
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    let decodeMsg json =
+            let decoder = Decode.object3 NewMessage
+                            ("id" := Decode.int)
+                            ("name" := Decode.string)
+                            ("msg" := Decode.string)
+                res     = Decode.decodeString decoder json
+            in case res of
+                   Ok m -> m
+                   Err s -> NoOp
+    in WebSocket.listen wsserver decodeMsg
+
+sendMsg : User.Model -> Cmd Msg
+sendMsg model =
+    let body = memberEncode model
+               |> Encode.encode 0
+        memberEncode model
+            = [ ("id", Encode.int model.id)
+              , ("name", Encode.string model.name)
+              , ("msg", Encode.string model.bufText)
+              ]
+              |> Encode.object
+    in WebSocket.send wsserver body
+
+wsserver : String
+wsserver = "ws://echo.websocket.org"
--- ui/src/User/User.elm
+++ ui/src/User/User.elm
@@ -3,10 +3,6 @@ module User.User exposing (..)
 import Html exposing (Html, text, div, button, input)
 import Html.Events exposing (onClick, onInput)
 import Html.Attributes exposing (type', value)
-import Http
-import Json.Encode as Encode
-import Json.Decode as Decode exposing ((:=))
-import WebSocket
 
 type alias UserId = Int
 
@@ -18,8 +14,6 @@ type alias Model =
 
 type Msg = SendMsg
          | Input String
-         | NewMessage Int String String
-         | NoOp
 
 view : Model -> Html Msg
 view model =
@@ -31,38 +25,6 @@ view model =
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        SendMsg -> (model, sendMsg model)
+        SendMsg -> (model, Cmd.none)
         Input bText -> ( { model | bufText = bText }
                        , Cmd.none )
-        NewMessage id name msg -> (model, Cmd.none)
-        NoOp -> (model, Cmd.none)
-
-wsserver : String
-wsserver = "ws://echo.websocket.org"
-
-sendMsg : Model -> Cmd Msg
-sendMsg model =
-    let body = memberEncode model
-               |> Encode.encode 0
-        memberEncode model
-            = [ ("id", Encode.int model.id)
-              , ("name", Encode.string model.name)
-              , ("msg", Encode.string model.bufText)
-              ]
-              |> Encode.object
-    in WebSocket.send wsserver body
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    WebSocket.listen wsserver decodeModel
-
-decodeModel : String -> Msg
-decodeModel json =
-    let decoder = Decode.object3 NewMessage
-                  ("id" := Decode.int)
-                  ("name" := Decode.string)
-                  ("msg" := Decode.string)
-        res     = Decode.decodeString decoder json
-    in case res of
-           Ok m -> m
-           Err s -> NoOp
